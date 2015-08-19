8c4754c36f305f8a0d6643c9433f56df142a502a
Leonardo Bartoli <leonardo.bartoli@gmail.com>
2015-08-19 20:44:02 +0200

    Clean up files lincense, added archive file to simplify extension install

--- extension.js
+++ extension.js
@@ -1,9 +1,21 @@
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version. <http://www.gnu.org/licenses/>
-*/
+/* This extension is a derived work of the Gnome Shell.
+*
+* Copyright (c) 2015 L. Bartoli
+*
+* This extension is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This extension is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this extension; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
 const Main = imports.ui.main;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
--- prefs.js
+++ prefs.js
@@ -1,10 +1,3 @@
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version. <http://www.gnu.org/licenses/>
- */
-
 const Gettext = imports.gettext;
 const _ = Gettext.gettext;
 const Gtk = imports.gi.Gtk;
