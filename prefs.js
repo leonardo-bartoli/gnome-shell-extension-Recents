acb8eaef763b3e36a5d802b12ad4c57aa37196b1
Leonardo Bartoli <leonardo.bartoli@gmail.com>
2015-08-24 19:24:06 +0200

    Shortcut imlementation
    Added preferences initial ui implementation to support shortcut.
    Updated gschemas to store the user-defined shortcut.

--- prefs.js
+++ prefs.js
@@ -1,10 +1,17 @@
 const Gettext = imports.gettext;
 const _ = Gettext.gettext;
+
 const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
+const St = imports.gi.St;
+
 const Lang = imports.lang;
 
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 const Settings = Me.imports.settings;
+
+const SHORTCUT_COLUMN_KEY  = 0;
+const SHORTCUT_COLUMN_MODS = 1;
 
 function init() {}
 
@@ -21,10 +28,83 @@ const RecetsPrefs = new Lang.Class({
 
         this._settings = settings;
 
+        this.add(new shortcutWidget(this._settings));
         this.add(new caseSensitiveWidget(this._settings));
         this.add(new fileFullPathWidget(this._settings));
         this.add(new itemsNumberWidget(this._settings));
         this.add(new widthWidget(this._settings));
+    }
+});
+
+const shortcutWidget = Lang.Class({
+    Name: 'shortcutWidget',
+    Extends: Gtk.Box,
+
+    _init: function(settings) {
+        this.parent({ orientation: Gtk.Orientation.HORIZONTAL });
+        this.set_baseline_position(Gtk.BaselinePosition.TOP);
+
+
+        this._label = new Gtk.Label({ label: _('Popup Shortcut'), xalign: 0 });
+
+
+
+        this._listStore = new Gtk.ListStore();
+        this._listStore.set_column_types([
+            GObject.TYPE_INT,
+            GObject.TYPE_INT
+        ]);
+        this._shortcutIter = this._listStore.append();
+        this._listStore.set(this._shortcutIter, [SHORTCUT_COLUMN_KEY, SHORTCUT_COLUMN_MODS], [0, 0]);
+
+        this._treeView = new Gtk.TreeView({
+            visible: true,
+            can_focus: true,
+            headers_visible: false,
+            search_column: 0,
+            model: this._listStore
+        });
+        this._treeView.set_enable_search(false);
+        this._treeView.set_search_column(-1);
+        
+        this._renderer = new Gtk.CellRendererAccel({editable: true});
+        this._renderer.connect('accel-edited', Lang.bind(this, function(renderer, path, key, mods, hwCode) {
+            let accel = Gtk.accelerator_name(key, mods);
+            
+            this._listStore.set(this._shortcutIter, [SHORTCUT_COLUMN_KEY, SHORTCUT_COLUMN_MODS], [key, mods]);
+        }));
+        this._renderer.connect("accel-cleared", Lang.bind(this, function() {
+            log('!!! cleared !!!');
+        }));
+        
+        let column = new Gtk.TreeViewColumn();
+
+        column.pack_start(this._renderer, false);
+        column.add_attribute(this._renderer, 'accel-key', SHORTCUT_COLUMN_KEY);
+        column.add_attribute(this._renderer, 'accel-mods', SHORTCUT_COLUMN_MODS);
+        this._viewport = new Gtk.Viewport({
+            visible: true,
+            can_focus: false,
+            hexpand: false,
+            vexpand: false
+        });
+        this._treeView.append_column(column);
+        this._viewport.child = this._treeView;
+        this._box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
+        this._box.pack_start(this._viewport, true, false, 0);
+
+        
+        this._button = Gtk.Button.new_from_icon_name('edit-delete-symbolic', Gtk.IconSize.BUTTON);
+        this._button.connect('clicked', Lang.bind(this, this._disableAccel));
+        
+	    this.pack_start(this._label, true, true, 0);
+        this.pack_end(this._button, false, false, 0);
+        this.pack_end(this._box, true, true, 10);
+    },
+
+    _disableAccel: function() {
+        this._listStore.set(this._shortcutIter, [SHORTCUT_COLUMN_KEY, SHORTCUT_COLUMN_MODS], [0, 0]);
+        
     }
 });
 
@@ -106,7 +186,7 @@ const widthWidget = Lang.Class({
 	    this.pack_start(this._label, true, true, 0);
 	    this.add(this._spin);
     }
-});
+});    
 
 function buildPrefsWidget() {
     let widget = new RecetsPrefs(new Settings.Settings(Me));
