327358ad5e6e94a0ca87a2df20fa75fe4159ac4b
Leonardo Bartoli <leonardo.bartoli@gmail.com>
2016-01-21 20:16:57 +0100

    Extension version 5

--- settings.js
+++ settings.js
@@ -4,26 +4,26 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 
 function getFromSchema(extension) {
-	let schema = 'org.gnome.shell.extensions.recents';
+    let schema = 'org.gnome.shell.extensions.recents';
 
-	const GioSSS = Gio.SettingsSchemaSource;
-	let schemaDir = extension.dir.get_child('schemas');
-	let schemaSource;
-	if (schemaDir.query_exists(null)) {
-		schemaSource = GioSSS.new_from_directory(schemaDir.get_path(),
-				                                 GioSSS.get_default(),
-				                                 false);
-	} else {
-		schemaSource = GioSSS.get_default();
-	}
+    const GioSSS = Gio.SettingsSchemaSource;
+    let schemaDir = extension.dir.get_child('schemas');
+    let schemaSource;
+    if (schemaDir.query_exists(null)) {
+        schemaSource = GioSSS.new_from_directory(schemaDir.get_path(),
+                                                 GioSSS.get_default(),
+                                                 false);
+    } else {
+        schemaSource = GioSSS.get_default();
+    }
 
-	let schemaObj = schemaSource.lookup(schema, true);
-	if (!schemaObj) {
-		throw new Error('Schema ' + schema + ' could not be found for extension ' +
-				extension.metadata.uuid + '. Please check your installation.');
-	}
+    let schemaObj = schemaSource.lookup(schema, true);
+    if (!schemaObj) {
+        throw new Error('Schema ' + schema + ' could not be found for extension ' +
+		                extension.metadata.uuid + '. Please check your installation.');
+    }
 
-	return new Gio.Settings({settings_schema: schemaObj});
+    return new Gio.Settings({settings_schema: schemaObj});
 }
 
 const Settings = new Lang.Class({
@@ -31,31 +31,31 @@ const Settings = new Lang.Class({
     Extends: Gio.Settings,
     ItemIconSize: 16,
     
-	Position: {
-		RIGHT: 0,
-		LEFT: 1,
+    Position: {
+        RIGHT: 0,
+        LEFT: 1
 	},
-    
+
     _init: function() {
         
 	    let schema = 'org.gnome.shell.extensions.recents';
         
-	    const GioSSS = Gio.SettingsSchemaSource;
-	    let schemaDir = Me.dir.get_child('schemas');
-	    let schemaSource;
-	    if (schemaDir.query_exists(null)) {
-		    schemaSource = GioSSS.new_from_directory(schemaDir.get_path(),
-				                                     GioSSS.get_default(),
-				                                     false);
-	    } else {
-		    schemaSource = GioSSS.get_default();
-	    }
+        const GioSSS = Gio.SettingsSchemaSource;
+        let schemaDir = Me.dir.get_child('schemas');
+        let schemaSource;
+        if (schemaDir.query_exists(null)) {
+            schemaSource = GioSSS.new_from_directory(schemaDir.get_path(),
+	                                                 GioSSS.get_default(),
+                                                     false);
+        } else {
+            schemaSource = GioSSS.get_default();
+        }
 
-	    let schemaObj = schemaSource.lookup(schema, true);
-	    if (!schemaObj) {
-		    throw new Error('Schema ' + schema + ' could not be found for extension ' +
-				            extension.metadata.uuid + '. Please check your installation.');
-	    }
+        let schemaObj = schemaSource.lookup(schema, true);
+        if (!schemaObj) {
+            throw new Error('Schema ' + schema + ' could not be found for extension ' +
+	                        extension.metadata.uuid + '. Please check your installation.');
+        }
 
         this.parent({settings_schema: schemaObj});
     },
@@ -73,7 +73,7 @@ const Settings = new Lang.Class({
 
     getPosition: function() {
         if (this.get_enum('position') === this.Position.LEFT) {        
-	        return 'left';
+            return 'left';
         }
 	        
         return 'right';
